b27548808634bea222b228dff54639e5efc70650
bblankk <jkookship@gmail.com>
2025-12-27 05:53:20 +0200

    Working on 'normalizing' the usable byte ranges! Later, pmm will have an easier time using them when they are page aligned!

--- srcCode/kernelModuleSrc/kernel/memap.c
+++ srcCode/kernelModuleSrc/kernel/memap.c
@@ -3,21 +3,22 @@
 #include "multiboot.h"
 #include "../include/memap.h"
 
-const char* mem_Type_Name(uint32_t type) {
-    switch (type) {
-        case 1: return "Available";
-        case 2: return "Reserved";
-        case 3: return "ACPI Reclaimable";
-        case 4: return "ACPI NVS";
-        case 5: return "Bad Memory";
-        default: return "Unknown";
-    }
-}
+// const char* mem_Type_Name(uint32_t type) {
+//     switch (type) {
+//         case 1: return "Available";
+//         case 2: return "Reserved";
+//         case 3: return "ACPI Reclaimable";
+//         case 4: return "ACPI NVS";
+//         case 5: return "Bad Memory";
+//         default: return "Unknown";
+//     }
+// }
 
+#define PAGE_SIZE 4096
 
-//memap_Region regions[32];
-//this is some kind of initializing function actually
-void print_Memory_Map(multibootInfo_t* mbInfo, memap_Region regions[],  size_t max_regions) {
+// parser , fills array (used for vmm/pmm)
+void print_Memory_Map(multibootInfo_t* mbInfo, memap_Region regions[],  size_t max_regions) 
+{
     //if no regions
     if (!(mbInfo->flags & (1 << 6))) {
        return 0;
@@ -40,4 +41,45 @@ void print_Memory_Map(multibootInfo_t* mbInfo, memap_Region regions[],  size_t m
     return regionCount;
     
 }
+
+//normalize usable ranges (making them "clean, page-aligned ranges") so pmm/vmm have an easier time with bookeeping and paging
+//clean up each range so the pages can be EXACT
+// Example after normalization:
+// Region A: start = 0x00100000, pages = 256
+// Region B: start = 0x01000000, pages = 1024
+// Region C: start = 0x08000000, pages = 8192
+
+uint64_t alignedBase =  base + (PAGE_SIZE - (base % PAGE_SIZE)) % PAGE_SIZE; //throw away partial range at the start
+
+uint64_t end = base + length;
+uint64_t alignedEnd = end - (end % PAGE_SIZE); //throw away partial page at the end
+ //normalizing function   ADD TO .H FILE LATER
+size_t memap_Normalize( multibootInfo_t* mbInfo, memap_Region regions[], size_t max_regions) 
+{
+    size_t count = 0;
+    uint32_t offset = 0;
+ while (offset < mbInfo->mmapLength && count < max_regions) 
+ {
+    uint32_t mmapAddr   = mbInfo->mmapAddr;
+    multibootMmapEntry_t* entry = (multibootMmapEntry_t*)(mmapAddr + offset);
+
+    if (entry->type == 1) { 
+
+        uint64_t base = entry->addr;
+        uint64_t end  = entry->addr + entry->len;
+        uint64_t alignedBase =base + (PAGE_SIZE - (base % PAGE_SIZE)) % PAGE_SIZE;
+        uint64_t alignedEnd = end - (end % PAGE_SIZE);
+
+            if (alignedEnd > alignedBase) {
+                regions[count].base  = alignedBase;
+                count++;
+            }
+        }
+
+        offset += entry->size + sizeof(entry->size);
+    }
+
+    return count;
+}
+
 
