3fbee5298079cdc611b9c7ecdafb8f5e92ea02c5
bblankk <jkookship@gmail.com>
2026-02-10 18:08:16 +0200

    bitmap initialization, part 100

--- srcCode/kernelModuleSrc/kernel/bitmap.c
+++ srcCode/kernelModuleSrc/kernel/bitmap.c
@@ -1,18 +1,30 @@
 
-// These functions are used in the kernel , sequentially AFTER memap initialization, which they rely on.
+/* These functions are used in the kernel , sequentially AFTER memap initialization, which they rely on.
+
+
+1 page = 4096 bytes (real memory)
+1 page = 1 bit (inside bitmap tracking) this is 1 PAGE inside the bitmap.  yes there are two incredibly, fundamentally, different meanings for the word 'page'. Yes they will be used interchangeably. Stay on your toes.
+1 bitmap byte = tracks 8 pages . yes you will have to track every bit in that byte. don't look at me like that, i didn't make the rules.
+
+example w 8 pages
+real mem:
+[4KB][4KB][4KB][4KB][4KB][4KB][4KB][4KB]
+
+Bitmap (1 byte = 8 bits):
+[1 0 0 1 1 0 0 0]
+
+*/
 
 
 
 
-
-#define PAGE_SIZE 4096 
-static uint8_t* pmm_bitmap;
-static uint64_t pmm_total_pages;
+#define PAGE_SIZE 4096 // a real memory page is 4kb, 4096 bytes.
+static uint64_t pmm_total_pages; // this will be used internally.
 
 
-// this function calculates the total pages of the final bitmap
-// param regions is a pointer to the start of the regions array
-// param regionCount is an unsigned integer for the total amount of expected safe-to-use regions
+/* this function calculates the total pages of the final bitmap
+ param regions is a pointer to the start of the regions array
+ param regionCount is an unsigned integer for the total amount of expected safe-to-use regions */
 uint64_t count_Pages(memap_Region regions[], uint32_t regionCount)
 {
     uint64_t pages = 0;
@@ -28,20 +40,51 @@ uint64_t count_Pages(memap_Region regions[], uint32_t regionCount)
 
 
 
-
+//this function initiates the bitmap with the total amount of pages and marks all pages as used (1) to be later marked as free (0) when they are found in the safe-to-use regions
 void bitmap_Init(memap_Region regions[], uint32_t regionCount, uint64_t bitmapBase)
 {
     pmm_total_pages = pmm_CountPages(regions, regionCount);
 
-    uint64_t bitmapBytes = (pmm_total_pages + 7) / 8;
+    uint64_t bitmapBytes = (pmm_total_pages + 7) / 8; //converts bits to bytes using some math ceil shenanighans. Adding 7 ensures that any remaining bits will be added to the final byte measurements.
 
-    bitmap = (uint8_t*)bitmapBase;
+    bitmap = (uint8_t*)bitmapBase; //what starts at memory address bitmapBase, that's going to be my bitmap array
 
     //mark all pages used (1)
      for (uint64_t i = 0; i < bitmapBytes; i++)
     {
-        bitmap[i] = 0xFF;
+        bitmap[i] = 0xFF; // every bit to 1, all pages start as reserved
     }
 
 
 }
+
+
+void bitmap_initFreePages(memap_Region regions[], uint32_t regionCount)
+{
+    for(uint32_t i =0 ; i< regionCount; i++)
+    {
+        if(region.type == 1) // if the region is usable
+        {
+            uint64_t start = regions[i].base;
+            uint64_t end   = regions[i].base + regions[i].length;
+            for (uint64_t addr = start; addr < end; addr += PAGE_SIZE) // loops every real mem page in the region. 
+                {
+                    uint64_t page_index = addr / PAGE_SIZE;
+                    uint64_t byte = page_index / 8;
+                    uint8_t  bit  = page_index % 8;
+
+                    bitmap[byte] &= ~(1 << bit); // more shenanighans. long story short, only the current bit(page flag) gets freed. long story below.
+                }
+        }
+    }
+}
+
+/* 
+explanation for line : bitmap[byte] &= ~(1 << bit)
+bitmap[byte] = 0b11111111   // all pages used
+bit = 3                       // page we want to free
+1 << 3    = 0b00001000
+~(1<<3)   = 0b11110111       // mask: flip that bit
+AND with bitmap:
+0b11111111 & 0b11110111 = 0b11110111 
+*/
